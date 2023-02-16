c1a3760f69eb96c371717a15f460793e0c22086f
Muhammed Shakir CK <shakirck@neoito.com>
2023-02-16 14:40:54 +0530

    feat: add migration block

---
+++ templates/migration.template.js
@@ -0,0 +1,18 @@
+// migrations/00_template.js
+
+const { Sequelize } = require("sequelize");
+
+async function up({ context: queryInterface }) {
+    /*
+        migration and seed logic goes here
+    */
+
+}
+
+async function down({ context: queryInterface }) {
+    /*
+        undo migration  logic goes here
+    */
+}
+
+module.exports = { up, down };
