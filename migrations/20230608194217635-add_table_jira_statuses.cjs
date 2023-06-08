2588c07da27deba85435751dd17c1c9678d10dd6
Sahilk111 <sahil@neoito.com>
2023-06-09 02:33:28 +0530

    add new table jira_status

---
+++ migrations/20230608194217635-add_table_jira_statuses.cjs
@@ -0,0 +1,47 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+async function up({ context: queryInterface }) {
+    /*
+        migration and seed logic goes here
+    */
+
+        await queryInterface.createTable('jira_status', {
+            id: {
+              type: Sequelize.INTEGER,
+              allowNull: false,
+              autoIncrement: true,
+              primaryKey: true,
+            },
+            project_id: {
+              type: Sequelize.INTEGER,
+              allowNull: false,
+              references: {
+                model: 'projects',
+                key: 'id'
+              }
+            },
+            status_id: {
+              type: Sequelize.STRING,
+              allowNull: false,
+            },
+            name: {
+              type: Sequelize.STRING,
+              allowNull: false,
+            },
+            jira_status_hash : {
+                type: Sequelize.STRING,
+                unique:true,
+                allowNull: false
+            }
+        })
+}
+
+async function down({ context: queryInterface }) {
+    /*
+        undo migration  logic goes here
+    */
+        await queryInterface.dropTable('jira_status');
+
+}
+
+module.exports = { up, down };
